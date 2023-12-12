014d165a323f441397715a9e3c38992426942e60
mydevops-manu <mydevops.manu@gmail.com>
2023-12-12 22:17:35 +0530

    added logs

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo "hello world"
+start_date=$(date)
+echo "script started at $start_date"
+
+echo "some lines are executed here...."
+end_date=$(date)
+echo "script ended at $end_date"
