cb2808551153107e10bf8a735568194b9493d66b
mydevops-manu <mydevops.manu@gmail.com>
2023-12-13 20:03:11 +0530

    added function in script

---
+++ 07-functions.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "checking root access..."
+ID=$(id -u)
+
+CHECK(){
+
+    if [ $1 -ne 0 ]
+    then
+        echo "ERROR:: $2 installation failed"
+        exit 1
+    else
+        echo "Successfully installed mysql"
+        $s --version
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: Please run script with root access"
+    exit 1
+else
+    echo "Root access confirmed..."
+    echo "Installing Mysql..."
+fi
+
+yum install mysql -y
+
+CHECK $? "mysql"
+
+yum install git -y
+
+CHECK $? "Git"
+
+
+
+
