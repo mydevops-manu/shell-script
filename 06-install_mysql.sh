4f6636844d00aa4630284d782dfacd53ad5c0150
mydevops-manu <mydevops.manu@gmail.com>
2023-12-13 19:25:07 +0530

    install mysql

--- 06-install_mysql.sh
+++ 06-install_mysql.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "checking root access..."
 ID=$(id -u)
 
 if [ $ID -ne 0 ]
@@ -7,5 +8,6 @@ then
     echo "ERROR:: Please run script with root access"
     exit 1
 else
+    echo "Root access confirmed..."
     echo "Installing Mysql..."
 fi
