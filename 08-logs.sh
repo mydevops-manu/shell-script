97097c84847433f589b36e62a13c778f1b28bf14
mydevops-manu <mydevops.manu@gmail.com>
2023-12-13 20:36:01 +0530

    added colors and logs

--- 08-logs.sh
+++ 08-logs.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
 
-echo "checking root access..."
+echo "checking root access..." &>> $LOGFILE
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
 CHECK(){
 
     if [ $1 -ne 0 ]
     then
-        echo -e "ERROR:: $2 installation $R failed $N"
+        echo -e "ERROR:: $2 installation $R failed $N" &>> $LOGFILE
         exit 1
     else
-        echo -e "$G Successfully $N installed $2"
+        echo -e "$G Successfully $N installed $2" &>> $LOGFILE
         $2 --version
     fi
 }
 
 if [ $ID -ne 0 ]
 then
-    echo -e "$R ERROR $N:: Please run script with root access"
+    echo -e "$R ERROR $N:: Please run script with root access" &>> $LOGFILE
     exit 1
 else
-    echo "Root access confirmed..."
-    echo "Installing Mysql..."
+    echo "Root access confirmed..." &>> $LOGFILE
+    echo "Installing Mysql..." &>> $LOGFILE
 fi
 
-yum install mysql -y
+yum install mysql -y &>> $LOGFILE
 
 CHECK $? "mysql"
 
-yum install git -y
+yum install git -y &>> $LOGFILE
 
 CHECK $? "git"
 
