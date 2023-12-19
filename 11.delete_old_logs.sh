1e83f22b8a6e0c1fcd2651859d1f487e93ba341f
mydevops-manu <mydevops.manu@gmail.com>
2023-12-19 19:15:16 +0530

    added delete_old_logs

---
+++ 11.delete_old_logs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[3em"
+N="\e[0m"
+
+LOG_DIR="/tmp/roboshopshell-logs"
+
+if [! -d $LOG_DIR]
+then
+    echo -e"$R ERROR $N:: LOG_DIR does not exist"
+fi
+
+FILES_TO_DELETE=$(find $LOG_DIR -type f -mtime +14 -name="*.log")
+
+WHILE IFS=read -r line
+do
+    echo "Deleting file: $line "
+done <<< $FILES_TO_DELETE
